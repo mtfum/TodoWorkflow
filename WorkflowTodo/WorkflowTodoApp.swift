bb37bad56df35cd4eabf83d415a0af70c536069d
mtfum <f.yamanaka1127@gmail.com>
2021-11-13 12:36:01 -0800

    Initial Commit

---
+++ WorkflowTodo/WorkflowTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowTodoApp.swift
+//  WorkflowTodo
+//
+//  Created by Fumiya Yamanaka on 2021/11/13.
+//
+
+import SwiftUI
+
+@main
+struct WorkflowTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
